54d0f99b9ae6dba892a5dad83aab89d429f9a74b
wavygoten <bossmanzach007@gmail.com>
2021-05-25 16:34:49 -0500

    added appspec/script files

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#backend start
+# python vclms/backend/manage.py runserver 2>&1 &
+
+#frontend start
+whoami
+whereis node
+which node
+whereis npm
+which npm
+
+cd /frontend
+
+npm start
